9c7730d0503e912f931d38da0a8a89aeba31b645
ItoShogo0819 <flaming.8634@icloud.com>
2026-06-18 16:20:46 +0900

    [add]入力判定を作成

---
+++ Assets/Player/PlayerScripts/InputDirection.cs
@@ -0,0 +1,34 @@
+namespace FightingGame.Inputs
+{
+    public enum AbsoluteDirection : byte
+    {
+        DownLeft = 1,
+        Down = 2,
+        DownRight = 3,
+        Left = 4,
+        Neutral = 5,
+        Right = 6,
+        UpLeft = 7,
+        Up = 8,
+        UpRight = 9
+    }
+
+    public enum RelativeDirection : byte
+    {
+        DownBack = 1,
+        Down = 2,
+        DownForward = 3,
+        Back = 4,
+        Neutral = 5,
+        Forward = 6,
+        UpBack = 7,
+        Up = 8,
+        UpForward = 9
+    }
+
+    public enum FacingDirection : sbyte
+    {
+        Left = -1,
+        Right = 1,
+    }
+}
