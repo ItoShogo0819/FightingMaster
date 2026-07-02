6584cf0a60717f14ba9b906f3d0cb4375121185c
ItoShogo0819 <flaming.8634@icloud.com>
2026-07-02 15:58:54 +0900

    [Update]入力蓄積ロジックの実装

--- Assets/System/Scripts/InputFrame.cs
+++ Assets/System/Scripts/InputFrame.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 namespace FightingGame.Inputs
 {
     /// <summary>
@@ -6,6 +8,7 @@ namespace FightingGame.Inputs
     public readonly struct InputFrame
     {
         public readonly int Frame;
+        public readonly Vector2 RawDirection;
         public readonly AbsoluteDirection AbsoluteDirection;
         public readonly RelativeDirection RelativeDirection;
         public readonly InputButton HeldButtons;
@@ -16,6 +19,7 @@ namespace FightingGame.Inputs
         /// 入力フレームのコンストラクタ
         /// </summary>
         /// <param name="frame"></param>
+        /// <param name="rawDirection"></param>
         /// <param name="absoluteDirection"></param>
         /// <param name="relativeDirection"></param>
         /// <param name="heldButtons"></param>
@@ -23,6 +27,7 @@ namespace FightingGame.Inputs
         /// <param name="releasedButtons"></param>
         public InputFrame(
             int frame,
+            Vector2 rawDirection,
             AbsoluteDirection absoluteDirection,
             RelativeDirection relativeDirection,
             InputButton heldButtons,
@@ -30,6 +35,7 @@ namespace FightingGame.Inputs
             InputButton releasedButtons)
         {
             Frame = frame;
+            RawDirection = rawDirection;
             AbsoluteDirection = absoluteDirection;
             RelativeDirection = relativeDirection;
             HeldButtons = heldButtons;
